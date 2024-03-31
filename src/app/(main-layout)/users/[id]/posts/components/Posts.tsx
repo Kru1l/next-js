87cb05db63b5bb0654a2e8527a60d8c752168d5a
Kru1l <paarl0814@gmail.com>
2024-03-31 23:42:51 +0300

    Initial commit

---
+++ src/app/(main-layout)/users/[id]/posts/components/Posts.tsx
@@ -0,0 +1,18 @@
+import {FC} from 'react';
+import {IPost} from "@/interfaces/postInterface";
+import {Post} from "@/app/(main-layout)/users/[id]/posts/Post";
+
+interface IProps {
+    posts: IPost[]
+}
+
+const Posts: FC<IProps> = ({posts}) => {
+
+    return (
+        <div>
+            {posts.map(post => <Post key={post.id} post={post}/>)}
+        </div>
+    );
+};
+
+export {Posts};
---
+++ src/app/(main-layout)/users/page.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+
+interface IProps {
+
+}
+
+const UsersPage: FC<IProps> = () => {
+
+    return (
+        <div>
+            UsersPage
+        </div>
+    );
+};
+
+export default UsersPage;
